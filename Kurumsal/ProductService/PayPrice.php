59d84b40d5f4e695e697afc90f87abbbf1d492aa
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-08-20 00:26:23 +0300

    Create PayPrice.php

---
+++ Kurumsal/ProductService/PayPrice.php
@@ -0,0 +1,10 @@
+<?php
+
+include "../../client.php";
+$client=new ggClient();
+$deneme = $client->payPrice('FT-hxGYCrx7cFS','xyz','zwg','5419xxxxxx2','128','06','12');
+ print_r ($deneme);
+
+
+
+?>
