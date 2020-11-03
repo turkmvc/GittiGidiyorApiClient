f6b5b485f87f3fe6b9a28c4d517c358833f5c9cc
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:03:31 +0300

    Update getProductVariants.php

--- Kurumsal/ProductService/getProductVariants.php
+++ Kurumsal/ProductService/getProductVariants.php
@@ -1,8 +1,8 @@
 <?php
-
+//Bu metod üründe yer alan varyantlar ile ilgili detay bilgilere ulaşmak için kullanılır.
 include "../../client.php";
 $client=new ggClient();
-$deneme = $client->getProductVariants(587955657);
+$deneme = $client->getProductVariants(598344340);
  print_r ($deneme);
 
 
