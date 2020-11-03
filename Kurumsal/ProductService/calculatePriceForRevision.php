eb0e81e66d0c99ac1b33d723488d196d6e50a925
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:02:48 +0300

    Update calculatePriceForRevision.php

--- Kurumsal/ProductService/calculatePriceForRevision.php
+++ Kurumsal/ProductService/calculatePriceForRevision.php
@@ -3,7 +3,7 @@
 include "../../client.php";
 $client=new ggClient();
 
-$deneme = $client->calculatePriceForRevision(587955657);
+$deneme = $client->calculatePriceForRevision('592463716', '2927');
  print_r ($deneme);
 
 
