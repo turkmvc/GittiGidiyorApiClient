ef155537f54218626fa9a9b8e78c8f5a492d41d3
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:03:35 +0300

    Update getProducts.php

--- Kurumsal/ProductService/getProducts.php
+++ Kurumsal/ProductService/getProducts.php
@@ -1,10 +1,10 @@
 <?php
-
+//GittiGidiyor, Hesabım/Ürün Yönetimi sayfasında yer alan: Aktif Satışlarım, Yeni Listelenenler, Satılanlar, Satılmayanlar bölümündeki ürün bilgilerine ulaşmak için kullanılır.
 include "../../client.php";
 $client=new ggClient();
-$deneme = $client->getProducts(0,3,'L',false);
+$deneme = $client->getProducts(0 ,10,'L',false);
  print_r ($deneme);
 
-
-
+//588289182
+ 
 ?>
