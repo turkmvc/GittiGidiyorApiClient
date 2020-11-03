77d596e584450cafec04464eb4e8b2f53162e583
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:03:16 +0300

    Update getProduct.php

--- Kurumsal/ProductService/getProduct.php
+++ Kurumsal/ProductService/getProduct.php
@@ -1,10 +1,9 @@
 <?php
 
+//Tek bir ürün ile ilgili detay bilgilere ulaşmak için kullanılır. Ürüne girilen bütün bilgilerin detayları bu method ile kontrol edilebilir.
+
 include "../../client.php";
 $client=new ggClient();
 $deneme = $client->getProduct(587955657);
  print_r ($deneme);
-
-
-
 ?>
