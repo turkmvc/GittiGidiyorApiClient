238a9910dae2f97f78f68b9252f79485ddb163fb
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:03:27 +0300

    Update getProductSpecs.php

--- Kurumsal/ProductService/getProductSpecs.php
+++ Kurumsal/ProductService/getProductSpecs.php
@@ -1,8 +1,8 @@
 <?php
-
+//Ürün özellikleri bilgilerini almak için erişilir.
 include "../../client.php";
 $client=new ggClient();
-$deneme = $client->getProductSpecs(58795565);
+$deneme = $client->getProductSpecs(588289182);
  print_r ($deneme);
 
 
