77ced3b61d001b4a3e51cdbeb9bbc9c75d9ac906
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:04:20 +0300

    Update relistProducts.php

--- Kurumsal/ProductService/relistProducts.php
+++ Kurumsal/ProductService/relistProducts.php
@@ -1,8 +1,9 @@
 <?php
-
+/*Satılmamış ürünleri tekrar satışa çıkartmak için kullanılır. 
+Satışmamış ürünler dışında sisteme yeni eklenen ancak henüz satışa açılmamış ücretsiz ilanların satışa açılması içinde kullanılabilir.*/
 include "../../client.php";
 $client=new ggClient();
-$deneme = $client->relistProducts([58795565]);
+$deneme = $client->relistProducts([599135363]);
  print_r ($deneme);
 
 
