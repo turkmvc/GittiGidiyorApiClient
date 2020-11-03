110bccd6bfe6b61c624b45497efcf39da9a4ef0d
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:03:29 +0300

    Update getProductsByIds.php

--- Kurumsal/ProductService/getProductsByIds.php
+++ Kurumsal/ProductService/getProductsByIds.php
@@ -1,8 +1,8 @@
 <?php
-
+//Birden fazla ürün ile ilgili detay bilgilere ulaşmak için kullanılır. Ürünlere girilen bütün bilgilerin detayları bu method ile kontrol edilebilir.
 include "../../client.php";
 $client=new ggClient();
-$deneme = $client->getProductsByIds([58795565]);
+$deneme = $client->getProductsByIds([588289182]);
  print_r ($deneme);
 
 
