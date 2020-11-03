64c0a435e851523f32ec674490fd279c490d0de0
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:02:38 +0300

    Update sendCargoInformation.php

--- Kurumsal/CargoService/sendCargoInformation.php
+++ Kurumsal/CargoService/sendCargoInformation.php
@@ -1,13 +1,7 @@
-
-
 <?php
-
 include "../../client.php";
 $client=new ggClient();
-
-$deneme = $client->sendCargoInformation(314740020001,12345,"mng","Türkiye","www.mngcargo.com","S");
+//İlgili satış kodu girildikten sonra, satış bilgisine kargo bilgisi eklemek için kullanılır. Bu metot satıcılar tarafından kullanılır.
+$deneme = $client->sendCargoInformation(314740020001,12345,"mng","Türkiye","www.mngcargo.com","S"); // salecode satış yapıldığı zaman aktif olucaktır.değer şimdiliktir.
  print_r ($deneme);
-
-
-
 ?>
