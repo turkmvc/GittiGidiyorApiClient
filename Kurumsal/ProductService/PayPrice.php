91160a521598d129d4ddcf0234d2796019f4affa
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:06:10 +0300

    Update PayPrice.php

--- Kurumsal/ProductService/PayPrice.php
+++ Kurumsal/ProductService/PayPrice.php
@@ -1,8 +1,11 @@
 <?php
-
+/*Listeleme servisi ve revizyon ücretlerini ödemek için kullanılır.
+ Kullanıcı, ödeme çeki ve kredi kartı bilgilerini kullanarak ödeme işlemini gerçekleştirir. 
+ Eğer kullanıcının ödeme çekinde ücretsiz ve ücreti bulunan ilanlar mevcut ise ücretsiz ilanlarda diğer ilanların ücreti ödenene kadar satışa alınmayacaktır. 
+ Kullanıcı toplu bir şekilde ücretli ilanları için ödeme yapabilir yada ücretsiz ilanlarını bu methodun dışında içerisinde ücretli bir ilan bulunmayacak şekilde listeleyebilir.*/
 include "../../client.php";
 $client=new ggClient();
-$deneme = $client->payPrice('FT-hxGYCrx7cFS','xyz','zwg','5419xxxxxx2','128','06','12');
+$deneme = $client->payPrice('FT-hxGYCrx7cFS','xyz','zwg','5419xxxxxx2','128','06','12'); // kredi kartı bilgileri girilecek.
  print_r ($deneme);
 
 
