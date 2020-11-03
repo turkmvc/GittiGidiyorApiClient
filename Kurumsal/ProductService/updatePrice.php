714380040d8a98b9db3924a3a63f6ecab3324918
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:04:03 +0300

    Update updatePrice.php

--- Kurumsal/ProductService/updatePrice.php
+++ Kurumsal/ProductService/updatePrice.php
@@ -1,9 +1,11 @@
 <?php
-
+//Ürün fiyat bilgilerini güncellemek için kullanılır.
 include "../../client.php";
 $client=new ggClient();
-$deneme = $client->updatePrice(587955657,"",4.80,false);
- print_r ($deneme);
+$deneme = $client->updatePrice($_POST['productId']," ",$_POST['price'],false);
+$updateMarketPrice = $client->updateMarketPrice($_POST['productId'],$_POST['marketPrice']);
+$updatePriceByPercentage = $client->updatePriceByPercentage($_POST['productId']," ",$_POST['operatorType'],$_POST['percentAge']);
+ print_r ($updatePriceByPercentage);
 
 
 
