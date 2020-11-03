e11b7f7ff0ad2dbbc5b1d820348be89f8006d82b
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:03:12 +0300

    Update finishEarly.php

--- Kurumsal/ProductService/finishEarly.php
+++ Kurumsal/ProductService/finishEarly.php
@@ -1,10 +1,9 @@
 <?php
 
+//Satıştaki ürünü erken sonlandırmak için kullanılır. İşlemin ardından kullanıcılara bilgilendirme amaçlı ürün sürelerinin bittiğine dair mail gönderilir.
+
 include "../../client.php";
 $client=new ggClient();
-$deneme = $client->finishEarly([587955657]);
+$deneme = $client->finishEarly([588289182]);
  print_r ($deneme);
-
-
-
 ?>
