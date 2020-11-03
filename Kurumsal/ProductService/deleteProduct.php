d37701763e71d64605a26e62508e2421a5f2bf80
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:03:14 +0300

    Update deleteProduct.php

--- Kurumsal/ProductService/deleteProduct.php
+++ Kurumsal/ProductService/deleteProduct.php
@@ -1,11 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+//Ürün/ürünleri silmek için kullanılır. ***************************
+    include "../../client.php";
+    $client=new ggClient();
 
-include "../../client.php";
-$client=new ggClient();
+    $deneme = $client->deleteProduct([$_POST['productID']]);
 
-$deneme = $client->deleteProduct([587955657]);
- print_r ($deneme);
+    if($_POST['productID']){
+        echo "Data silindi.";
+    }else{
+        echo "Bir Problem olustu";
+    }
+    ?>
+    <br>
+    <br>
+    <br>
+    
+    <a  href="http://localhost/gittigidiyor/Kurumsal/ProductService/getProducts.php">Product silme sayfasına geri dön.</a>
 
+</body>
 
-
-?>
+</html>
