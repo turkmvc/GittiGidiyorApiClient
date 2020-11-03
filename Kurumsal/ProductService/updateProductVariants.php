dd48492361aa340e8edf660ecb92d0f61256d3c7
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:04:10 +0300

    Update updateProductVariants.php

--- Kurumsal/ProductService/updateProductVariants.php
+++ Kurumsal/ProductService/updateProductVariants.php
@@ -1,8 +1,8 @@
 <?php
-
+//Bu metod ürünün varyant bilgilerini güncellemek için kullanılır.
 include "../../client.php";
 $client=new ggClient();
-$deneme = $client->updateProductVariants(58795565);
+$deneme = $client->updateProductVariants("598344340","","U");
  print_r ($deneme);
 
 
