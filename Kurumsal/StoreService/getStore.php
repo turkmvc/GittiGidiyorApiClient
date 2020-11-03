a01976986667a5e281a0fe58fef564f5a04e0198
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:05:39 +0300

    Update getStore.php

--- Kurumsal/StoreService/getStore.php
+++ Kurumsal/StoreService/getStore.php
@@ -3,6 +3,7 @@
 include "../../client.php";
 $client=new ggClient();
 $deneme = $client->getStore();
+//Bu metot, mağaza tipi ve başlangıç/bitiş tarihleri gibi bilgilere ulaşmak için kullanılır.
  print_r ($deneme);
 
 
