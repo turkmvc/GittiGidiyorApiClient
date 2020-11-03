6cbb22078934d1e1b1cded618efa6c238488fe0d
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:01:41 +0300

    Update deleteApplication.php

--- anonim/ApplicationService/deleteApplication.php
+++ anonim/ApplicationService/deleteApplication.php
@@ -3,7 +3,8 @@
 include "../client.php";
 $client=new ggClient();
 $client->deleteApplication("ASzvDFJ42mPRXphqtmPY");
-
+//Bu metot, geliştirici tarafından oluşturulmuş uygulamaların silinmesi için kullanılır.
 print_r($client);
+
 
 ?>
