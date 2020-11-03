51421ae2c279e5cdb23342196671d02f39c078f0
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:03:33 +0300

    Update getProductStatuses.php

--- Kurumsal/ProductService/getProductStatuses.php
+++ Kurumsal/ProductService/getProductStatuses.php
@@ -1,10 +1,7 @@
 <?php
-
+//Bu metod ürünlerin Aktif Satışlarım, Yeni Listelenenler, Satılanlar veya Satılmayanlar bölümlerinden hangisinde olduğu bilgisine ulaşmak için kullanılır.
 include "../../client.php";
 $client=new ggClient();
-$deneme = $client->getProductStatuses([58795565]);
+$deneme = $client->getProductStatuses([588289182]);
  print_r ($deneme);
-
-
-
 ?>
