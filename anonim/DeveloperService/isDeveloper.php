7de9111f498db5ed260855227c9b6b6ac40d8c58
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:03:47 +0300

    Update isDeveloper.php

--- anonim/DeveloperService/isDeveloper.php
+++ anonim/DeveloperService/isDeveloper.php
@@ -1,10 +1,12 @@
 
 <?php
 
-include "../client.php";
+include "../../client.php";
 $client=new ggClient();
-$client->isDeveloper("gamzesirakaya736044","130199ga");
+$a=$client->isDeveloper("gamzesirakaya736044","130199ga");
+//Herhangi bir GittiGidiyor kullanıcısının geliştirici olup olmadığını kontrol etmek için bu metot kullanılır.
 
-print_r($client);
+
+print_r($a);
 
 ?>
