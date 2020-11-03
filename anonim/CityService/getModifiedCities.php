13d5ff560c2c447ef7340bdc94d416ccf94c4ab7
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:02:02 +0300

    Update getModifiedCities.php

--- anonim/CityService/getModifiedCities.php
+++ anonim/CityService/getModifiedCities.php
@@ -3,7 +3,7 @@
 include "../client.php";
 $client=new ggClient();
 $client-> getModifiedCities();
-
+/* Bu metot, belirtilen tarihten şimdiki zamana kadar şehir adları, kodları değişmiş olan şehirleri listelemek için kullanılır. Bu metodu kullanarak, değişiklikleri güncellemek için her seferinde bütün şehirlere ait veriyi çekmek yerine, sadece belirtilen tarihten sonra gerçekleşmiş olan değişiklikleri çekebilirsiniz.*/
 print_r($client);
 
 ?>
