2acd7e47fccf79ea45dc2ff4f577f8085f011b1f
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:03:38 +0300

    Update InsertProduct.php

--- Kurumsal/ProductService/InsertProduct.php
+++ Kurumsal/ProductService/InsertProduct.php
@@ -1,9 +1,91 @@
 <?php
-
+//Gittigidiyor üzerinde ürün listelemek için bu metot kullanılır. 
+//Eklenen ürünler GittiGidiyor sayfasında Hesabım/Ürün Yönetimi/Listelemeye Hazır Ürünler kısmında gözükecektir.
 include "../../client.php";
-$client=new ggClient();
-$xml=
-'<product>  <categoryCode>gdk</categoryCode>  <storeCategoryId></storeCategoryId>  <title>Kodak Z915 12MP</title>  <subtitle></subtitle>  <specs>  <spec name="Durumu" value="Kullanılmış" type="Combo" required="true"/>  <spec name="Çözünürlük (Megapiksel)" value="10.0 - 11.9 MP" type="Combo" required="true"/>  <spec name="Marka" value="Kodak" type="Combo" required="true"/>  </specs>  <photos>  <photo photoId="0">  <url>http://images.gittigidiyor.com/2941/KODAK-Z915-10MP-10X-ZOOM-HD-SIFIRURUN__29416457_0.jpg</url>  </photo>  </photos>  <pageTemplate>1</pageTemplate>  <description>Kodak Z915 Fotograf Makinesi 10Mp 50X zoom</description>  <startDate></startDate>  <catalogId></catalogId>  <catalogDetail></catalogDetail>  <format>S</format>  <startPrice></startPrice> <buyNowPrice>240</buyNowPrice>  <netEarning></netEarning>  <listingDays>30</listingDays>  <productCount>1</productCount>  <cargoDetail>  <city>34</city>  <cargoCompanies>  <cargoCompany>aras</cargoCompany>  </cargoCompanies>  <shippingPayment>B</shippingPayment>  <cargoDescription></cargoDescription>  <shippingWhere>country</shippingWhere>  </cargoDetail>  <affiliateOption>false</affiliateOption>  <boldOption>false</boldOption>  <catalogOption>false</catalogOption>  <vitrineOption>false</vitrineOption>  </product>';
+$client = new ggClient();
 
-$deneme = $client->insertProducts(null,$xml,false,false);
- print_r ($deneme);
+
+$xml =
+    ' <product>
+    <categoryCode>'.$_POST['categorycode'].'</categoryCode>
+    <storeCategoryId>0</storeCategoryId>
+    <title>'.$_POST['title'].'</title>
+    <specs>
+       <spec required="false" value="'.$_POST['marka'].'" name="Marka"/>
+       <spec required="false" value="'.$_POST['value1'].'" name="Durum"/>
+    </specs>
+    <photos>
+       <photo photoId="0">
+          <url>'.$_POST['url'].'</url>
+       </photo>
+       <photo photoId="1">
+          <url>'.$_POST['url1'].'</url>
+       </photo>
+      
+    </photos>
+    <pageTemplate>2</pageTemplate>
+    <description>'.$_POST['description'].'</description>
+    <catalogId>0</catalogId>
+    <catalogDetail>0</catalogDetail>
+    <format>'.$_POST['format'].'</format>
+    <buyNowPrice>'.$_POST['buyNowPrice'].'</buyNowPrice>
+    <listingDays>'.$_POST['listiningdays'].'</listingDays>
+    <productCount>5</productCount>
+    <cargoDetail>
+       <city>'.$_POST['City'].'</city>
+       <cargoCompanies>
+          <cargoCompany>'.$_POST['CargoCompany'].'</cargoCompany>
+       </cargoCompanies>
+       <shippingPayment>'.$_POST['ShippingPayment'].'</shippingPayment>
+       <shippingWhere>'.$_POST['Shippiningwhere'].'</shippingWhere>
+       <cargoCompanyDetails>
+          <cargoCompanyDetail>
+             <name>aras</name>
+             <value>aras</value>
+             <cityPrice>'.$_POST['cityPrice'].'</cityPrice>
+             <countryPrice>'.$_POST['countryPrice'].'</countryPrice>
+
+          </cargoCompanyDetail>
+       </cargoCompanyDetails>
+       <shippingTime>
+          <days>'.$_POST['days'].'</days>
+       </shippingTime>
+    </cargoDetail>
+    <affiliateOption>false</affiliateOption>
+    <boldOption>false</boldOption>
+    <catalogOption>false</catalogOption>
+    <vitrineOption>false</vitrineOption>
+    <variantGroups>
+       <variantGroup alias="Renk:Zümrüt" valueId="962183"  nameId="2927" >
+          <variants>
+             <variant>
+                <variantSpecs>
+                   <variantSpec specDataOrderNumber="820" orderNumber="3" value="Altın/Dore" valueId="'.$_POST['variantSpecvalueId'].'" name="Renk" nameId="2927"/>
+                   <variantSpec specDataOrderNumber="2" orderNumber="4" value="2" valueId="760374" name="Yaş" nameId="2926"/>
+                </variantSpecs>
+                <quantity>'.$_POST['quantity'].'</quantity>
+                <stockCode/>
+                <soldCount>'.$_POST['soldCount'].'</soldCount>
+             </variant>
+          </variants>
+          <photos>
+             <photo photoId="0">
+                <url>https://mcdn01.gittigidiyor.net/53642/536423161_0.jpg</url>
+             </photo>
+          </photos>
+       </variantGroup>
+     
+    </variantGroups>
+    <marketPrice>'.$_POST['marketprice'].'</marketPrice>
+    <manufacturerPartNo/>
+ </product>
+
+ ';
+
+ //$xmll = simplexml_load_string($xml);
+//$json = json_encode($xmll);
+//$array = json_decode($json,TRUE);
+//print_r($array);
+//592463716 productıd
+$deneme = $client->insertProducts(null, $xml, false, false);
+print_r($deneme);
