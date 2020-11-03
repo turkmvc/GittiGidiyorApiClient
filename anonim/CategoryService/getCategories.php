13667a8386ce5b988edb60fbf23d6f90d3b8173e
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:01:48 +0300

    Update getCategories.php

--- anonim/CategoryService/getCategories.php
+++ anonim/CategoryService/getCategories.php
@@ -1,10 +1,16 @@
 <?php
 
-include "../client.php";
+include "../../client.php";
 $client=new ggClient();
-$deneme = $client->getCategories(false,false,false);
- print_r ($deneme);
+$deneme = $client->getCategories(0,100,false,false,false);
+
+$b=($deneme->categories);
+//GittiGidiyor da yer alan bütün kategori bilgilerine ulaşmak için kullanılır. Ürün ekleme işlemi sırasında ilgili kategori bilgisinin belirtilmesi gerektiği için bu bilginin gönderilmesi zorunludur.
 
 
+print_r($b);
 
+  /* $array = json_decode($deneme, true);
+   
+   print_r( $array);*/
 ?>
