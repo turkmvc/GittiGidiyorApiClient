82c2587741b6dce9e9dbd11272bf6d2e81d8ed70
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:02:45 +0300

    Update getCargoInformation.php

--- Kurumsal/CargoService/getCargoInformation.php
+++ Kurumsal/CargoService/getCargoInformation.php
@@ -1,10 +1,10 @@
 
 <?php
-
+//Bu metot, satış kodunu girdikten sonra mevcut satışın kargo bilgisini girmek için kullanılır.
 include "../../client.php";
 $client=new ggClient();
 
-$deneme = $client->getCargoInformation(6901443257119);
+$deneme = $client->getCargoInformation(6901443257119); // salecode satış yapıldığı zaman aktif olucaktır.değer şimdiliktir.
  print_r ($deneme);
 
 
