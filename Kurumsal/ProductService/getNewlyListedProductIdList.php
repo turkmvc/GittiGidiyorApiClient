bdaf2f9d7384a8194c4f1187915442b34a67daed
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:03:23 +0300

    Update getNewlyListedProductIdList.php

--- Kurumsal/ProductService/getNewlyListedProductIdList.php
+++ Kurumsal/ProductService/getNewlyListedProductIdList.php
@@ -1,9 +1,10 @@
 <?php
 
+//GittiGidiyor da "Yeni Listelenenler" bölümündeki ürünlere ulaşmak için kullanılır.
+
 include "../../client.php";
 $client=new ggClient();
-
-$deneme = $client->getNewlyListedProductIdList(0,10,true);
+$deneme = $client->getNewlyListedProductIdList(0,32,true);
  print_r ($deneme);
 
 
