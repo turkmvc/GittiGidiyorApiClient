17eeb1c109e4252c3a63b6c7ab9fb161fdc8947c
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:02:06 +0300

    Update getCities.php

--- anonim/CityService/getCities.php
+++ anonim/CityService/getCities.php
@@ -1,8 +1,9 @@
 <?php
 
-include "../client.php";
+include "../../client.php";
 $client=new ggClient();
-$a=$client->getCities(0,3);
+$a=$client->getCities(0,82);
+//GittiGidiyor daki bütün şehir bilgilerine ulaşmak için kullanılır. Ürün ekleme işlemi sırasında ilgili şehir bilgisinin (şehir adları ve kodları) belirtilmesi gerektiği için bu bilginin gönderilmesi zorunludur.
 
 print_r($a);
 
