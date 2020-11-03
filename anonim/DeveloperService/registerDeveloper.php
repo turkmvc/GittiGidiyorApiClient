992aed722f2663b8a659bd3ead6585b6689427f1
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:02:19 +0300

    Update registerDeveloper.php

--- anonim/DeveloperService/registerDeveloper.php
+++ anonim/DeveloperService/registerDeveloper.php
@@ -2,8 +2,8 @@
 
 include "../client.php";
 $client=new ggClient();
-$client->registerDeveloper("gamzesirakaya736044","130199ga");
-
-print_r($client);
+$a=$client->registerDeveloper("gamzesirakaya736044","130199ga");
+//GittiGidiyor API kullanıcısını, "geliştirici" olarak sisteme kaydetmek için bu metot çağırılmalıdır. Bu metodun döndürmüş olduğu Developer ID vasıtasıyla uygulamalar yaratılabilir.
+print_r($a);
 
 ?>
