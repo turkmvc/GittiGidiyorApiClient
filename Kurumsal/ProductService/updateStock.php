950334ba5b39520f345dd731e9aab906286cd39a
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:06:06 +0300

    Update updateStock.php

--- Kurumsal/ProductService/updateStock.php
+++ Kurumsal/ProductService/updateStock.php
@@ -1,9 +1,9 @@
 <?php
-
+//Stok bilgilerini güncellemek için kullanılır. Stok 0 olarak tanımlanır ise ürünün stoğu bulunmadığı için ürün kapatılır.
 include "../../client.php";
 $client=new ggClient();
-$deneme = $client->updateStock(587955657,"",200,false);
- print_r ($deneme);
+$deneme = $client->updateStock(587953353,"",10,false);
+ print_r($deneme);
 
 
 
