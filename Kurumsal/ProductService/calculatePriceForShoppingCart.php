50abcb0e7ea7e5a1a11893339a2f8dc96955b083
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:02:52 +0300

    Update calculatePriceForShoppingCart.php

--- Kurumsal/ProductService/calculatePriceForShoppingCart.php
+++ Kurumsal/ProductService/calculatePriceForShoppingCart.php
@@ -1,11 +1,9 @@
 <?php
 
+//Bu metot, Listelemeye Hazır Ürünler alanında bulunan ürünleri Aktif Satışlar'a almak için kullanılır.
+
 include "../../client.php";
 $client=new ggClient();
-
-$deneme = $client->calculatePriceForShoppingCart([587955657]);
+$deneme = $client->calculatePriceForShoppingCart(array('588289182'),null); // dönen hata kredikartı bilgisi tam olarak girilmediğinden dolayı.
  print_r ($deneme);
-
-
-
 ?>
