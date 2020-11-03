6682d1bc0a71502a7f4acb2848d527080385fd78
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-04 00:03:45 +0300

    Update getStockAndPrice.php

--- Kurumsal/ProductService/getStockAndPrice.php
+++ Kurumsal/ProductService/getStockAndPrice.php
@@ -1,8 +1,8 @@
 <?php
-
+//Ürün yada stok numarası girilen ürünlerin fiyat ve stok bilgilerinin alınması için kullanılır.
 include "../../client.php";
 $client=new ggClient();
-$deneme = $client->getStockAndPrice([58795565]);
+$deneme = $client->getStockAndPrice([598876311]);
  print_r ($deneme);
 
 
